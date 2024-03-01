78f1c8b4b43517ac7db6116717a6a68ced984ce2
HighBible <stitch3514@gmail.com>
2024-03-02 00:05:10 +0900

    header-trending edited

--- js/header.js
+++ js/header.js
@@ -1,3 +1,13 @@
+// import { getMoviesPopular } from './main.js';
+// async function fetchData() {
+//         const data = await getMoviesPopular();
+//         console.log(data.results, "ddd");
+// }
+
+// fetchData();
+
+
+
 const header = document.getElementById('ko-header')
 const btnProfile = document.querySelector('.ko-btn-account')
 const btnSearch = document.querySelector('.ko-btn-search')
@@ -9,7 +19,55 @@ const searchArea = document.querySelector('.ko-search-area')
 const body = document.querySelector('body')
 const searchInput = document.getElementById('search_area')
 const recentBox = document.querySelector('.ko-search-data')
+const trendingBox = document.querySelector('.ko-trending-box')
 let toggle = true;
+let today = new Date();   
+
+let movieList = [];
+let url = new URL(`https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1`);
+const IMG_URL = `http://image.tmdb.org/t/p/w500`;
+
+
+
+const getMoviesTrending = async () => {
+  url = new URL(`https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1`);
+  const options = {
+      method: 'GET',
+      headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDc2OGMxZTdlYWJmYWI5Y2Q5NGFiNzQyMjNhZjg1YyIsInN1YiI6IjY1ZGQ0NjZjMmFjNDk5MDE3ZGNhZGZjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xtPAVAUiJC6-xfEkO9tnDb_UHPDTIo3bJaKtMLNdMkg'
+      }
+  };
+  const response = await fetch(url, options);
+  const data = await response.json();
+  movieList = data.results;
+  // render();
+  // getMovies(data);
+  // console.log(data.results)
+  movieList = data.results
+  renderTrendingList()
+  return data;
+}
+
+getMoviesTrending()
+
+
+console.log(today.toLocaleString())
+ 
+
+const renderTrendingList = () => {
+  console.log('render', movieList)
+  for(let i=0; i<10; i++) {
+    let movieTitle = movieList[i].title
+    trendingBox.innerHTML += `
+    <li class="ko-search-content">
+      <a href="#"><span class="red">${i+1}</span>${movieTitle}</a>
+      </li>`
+  }
+  document.querySelector('.ko-search-time').textContent = `${today.toLocaleString()} 기준`
+} 
+
+
 
 
 console.log(btnSearchImg)
