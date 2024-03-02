4a20b4b80f31aa383849ec65dbd9fa1f97662ad0
tw990106 <118413280+tw990106@users.noreply.github.com>
2024-03-03 01:52:11 +0900

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // import { sendDetail } from './main.js';
 const urlParams = new URLSearchParams(window.location.search);
 const movieId = urlParams.get('id');
-const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNDc2OGMxZTdlYWJmYWI5Y2Q5NGFiNzQyMjNhZjg1YyIsInN1YiI6IjY1ZGQ0NjZjMmFjNDk5MDE3ZGNhZGZjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xtPAVAUiJC6-xfEkO9tnDb_UHPDTIo3bJaKtMLNdMkg';
+const API_KEY = '6f97625a1c75f3ce06489a0e5b0ebda1';
 const options = {
     method: 'GET', 
     headers: {accept: 'application/json'
