845ddea3de4401490aa6908d1b4fae6aa5a5d30c
HighBible <stitch3514@gmail.com>
2024-03-02 22:35:11 +0900

    once fn edited

--- js/header.js
+++ js/header.js
@@ -84,10 +84,30 @@ const renderTrendingList = () => {
     }
    
   document.querySelector('.ko-search-time').textContent = `${today.toLocaleString()} 기준`
-  return
 } 
 
-trendingMovies()
+
+
+
+function once(fn, context) { 
+	var result;
+
+	return function() { 
+		if(fn) {
+			result = fn.apply(context || this, arguments);
+			fn = null;
+		}
+
+		return result;
+	};
+}
+
+// Usage
+var canOnlyFireOnce = once(trendingMovies());
+
+
+
+
 
 btnProfile.addEventListener('mouseover', function(){
   profileBox.style.display = 'block'
