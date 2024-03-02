6c2f6cf641ebf7154ac533a14690c2aded66b759
tw990106 <118413280+tw990106@users.noreply.github.com>
2024-03-03 08:40:45 +0900

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -70,7 +70,7 @@ getSimilarMovie();
 const render = () => {
     // const director = movieDetail.credits ? movieDetail.credits.crew.find(person => person.job === 'Director') : null;
     // const cast = movieDetail.credits ? movieDetail.credits.cast.map(actor => actor.name).join(', ') : '정보 없음';
-    let overviewHTML = movieDetail.overview ? `<p class="summary mb-2">${movieDetail.overview}</p>` : '<p class="summary mb-2">줄거리가 없습니다.</p>';
+    const overviewHTML = movieDetail.overview ? `<p class="summary mb-2">${movieDetail.overview}</p>` : '<p class="summary mb-2">줄거리가 없습니다.</p>';
 
     const detailHTML = `
     <div class="detail-img"><img src="${IMG_URL}${movieDetail.poster_path}" alt="포스터"></div>
@@ -101,7 +101,7 @@ const render = () => {
 }
 
 const similarRender = (movieList) => {
-    overviewHTML = movieDetail.overview ? `<p class="summary mb-2">${movieDetail.overview}</p>` : '<p class="summary mb-2">줄거리가 없습니다.</p>';
+    const overviewHTML = movieDetail.overview ? `<p class="summary mb-2">${movieDetail.overview}</p>` : '<p class="summary mb-2">줄거리가 없습니다.</p>';
     
     const similarHTML = movieList.map(movie => `
     <li class="swiper-slide">
