19062c3e543c474a474025107a994d7f64456e2c
tw990106 <118413280+tw990106@users.noreply.github.com>
2024-03-02 23:24:17 +0900

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -148,35 +148,34 @@ const render = (movieDetail) => {
 }
 
 // 비슷한 영화 목록
-const fetchSimilarMovies = async () => {
-    try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/similar?language=ko&api_key=${API_KEY}`);
-        const data = await response.json();
-        return data.result;
-    }catch(error) {
-        console.error('Error fetching similar movies', error);
-        return [];
-    }
+const getSimilarMovies = async () => {
+    url = new URL(`https://api.themoviedb.org/3/movie/${movieId}/similar?language=ko&api_key=${API_KEY}`);
+    const options = {
+        method: 'GET',
+        headers: {accept: 'application/json'
+        }
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    movieList = data.results;
+    similarRender(data);
+    return data;
 }
 
 const similarRender = async () => {
-    try{
-        const similarMovies = await fetchSimilarMovies();
-        if (similarMovies && similarMovies.length > 0){
-            const similarHTML = similarMovies.map(movieDetail => `
-                <li class="swiper-slide">
-                    <img src="${IMG_URL}${movieDetail.poster_path}" alt="${movieDetail.title}">
-                    <div class="list-txt">
-                        <h5>${movieDetail.title}</h5>
-                        <span>${movieDetail.genres.map(genre => genre.name).join(', ')}</span>
-                        ${movieDetail.overview ? `<p class="summary mb-2">${movieDetail.overview}</p>` : '<p class="summary mb-2">줄거리가 없습니다.</p>'}
-                    </div>
-                </li>
-            `);
-            document.getElementById('similar-list').innerHTML = similarHTML.join('');
-        }else {
-            console.log('No similar movies found.');
-        }
+    const similarHTML = movieList.map(movie => `
+        <li class="swiper-slide">
+            <img src="${IMG_URL}${movie.poster_path}" alt="${movie.title}">
+            <div class="list-txt">
+                <h5>${movie.title}</h5>
+                <span>${movie.genres.map(genre => genre.name).join(', ')}</span>
+                ${movie.overview ? `<p class="summary mb-2">${movie.overview}</p>` : '<p class="summary mb-2">줄거리가 없습니다.</p>'}
+            </div>
+        </li>
+    `);
+    document.getElementById('similar-list').innerHTML = similarHTML.join('');
+}
         // const similarHTML = similarMovies.map(movie => `
         // <li class="swiper-slide">
         //         <img src="${IMG_URL}${movie.poster_path}" alt="${movie.title}">
@@ -188,13 +187,9 @@ const similarRender = async () => {
         //     </li>
         // `).join('');
         // document.getElementById('similar-list').innerHTML = similarHTML;    
-    } catch(error){
-        console.error('Error rendering similar movies', error);
-    }
-}
-
-similarRender();
-
+    
+// similarRender();
+getSimilarMovies();
 
 /*
 import {sendDetail} from './main.js';
